40bb1930358a10f7776456c71171771e3e0634c0
MilanSas <milansas1@gmail.com>
2022-12-13 12:47:20 +0100

    Avoiding keyboard more

--- src/hooks/useLayoutDropdown.js
+++ src/hooks/useLayoutDropdown.js
@@ -14,7 +14,7 @@ export const useLayoutDropdown = (data, dropdownStyle, rowStyle, search) => {
   }); // dropdown height
   const [dropdownWIDTH, setDropdownWIDTH] = useState(0); // dropdown width
   const remainigHeightAvoidKeyboard = useKeyboardRemainingScreenHeight();
-  const safeDropdownViewUnderKeyboard = rowStyle && rowStyle.height ? rowStyle.height * 3 : 50 * 3;
+  const safeDropdownViewUnderKeyboard = rowStyle && rowStyle.height ? rowStyle.height * 6 : 50 * 6;
 
   useEffect(() => {
     setDropdownHEIGHT(calculateDropdownHeight(dropdownStyle, rowStyle, data?.length || 0, search));
